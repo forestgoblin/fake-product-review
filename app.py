6f534cad36f205f11acd3b54b0b75f4b9283552f
forestgoblin <93105423+forestgoblin@users.noreply.github.com>
2023-12-07 02:58:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 
 import pickle
 
-svm_classifier_loaded = pickle.load(open('/main/svm_classifier_saved.pkl', 'rb'))
+svm_classifier_loaded = pickle.load(open('svm_classifier_saved.pkl', 'rb'))
 
-naive_bayes_loaded = pickle.load(open('/main/naive_bayes_saved.pkl', 'rb'))
+naive_bayes_loaded = pickle.load(open('naive_bayes_saved.pkl', 'rb'))
 
 def main():
     st.title("Simple Streamlit Application")
